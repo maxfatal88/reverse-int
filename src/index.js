d78515394c270e3cca11f7caff205b75a4728601
Yavorskiy Maksim <maxfatal@mail.ru>
2023-01-11 18:53:00 +0300

    resolved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
   
+let=stringNum=String(n);
+
+if (stringNum[0]==='-') {
+    stringNum=stringNum.slice(1);
+}
+
+if (stringNum[stringNum.length-1]==='0') {
+    stringNum=stringNum.slice(0,-1);
+}
+
+return +stringNum.split("").reverse().join("");
+
 }
